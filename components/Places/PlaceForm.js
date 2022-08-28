866557a4b8ed47c85353785cd412897d47f8335c
Juciano <juciano.junior@hotmail.com>
2022-08-28 20:46:30 +0100

    feat: basic form components

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from "react";
+import { ScrollView, Text, TextInput, View, StyleSheet } from "react-native";
+
+import { Colors } from "../../constants/colors";
 
 const PlaceForm = () => {
-  return (
-    <View>
-        <Text>
-            The Place Form
-        </Text>
-    </View>
-  )
-}
+  const [enteredTitle, setEnteredTitle] = useState("");
 
-export default PlaceForm
+  function changeTitleHandler(enteredTitle) {
+    setEnteredTitle(enteredTitle);
+  }
+
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChange={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
